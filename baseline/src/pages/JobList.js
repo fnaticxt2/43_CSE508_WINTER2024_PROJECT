d013122bbc4e96e2091a43442a777414f2dad2ac
dhruv23056 <142231618+dhruv23056@users.noreply.github.com>
2024-04-23 22:18:36 +0530

    Add files via upload

--- baseline/src/pages/JobList.js
+++ baseline/src/pages/JobList.js
@@ -1,38 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap'; // Import Bootstrap components for styling
+import backgroundImage from '../assets/images/job.jpg';
 
 const JobList = () => {
-  const [jobs, setJobs] = useState([])
+  const [jobs, setJobs] = useState([]);
   const location = useLocation();
 
   useEffect(() => {
     if (location.state && location.state.jobdata) {
       const jobData = location.state.jobdata;
       setJobs(jobData);
-      console.log(jobData);
     }
   }, [location]);
-  
+
   return (
-    <div className="container mt-4">
-      <h2 className="text-center mb-4">Jobs</h2>
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-            
-        {jobs.map((job) => (
-          <div key={job[0]}>
-          <a href={job[8]} target='_BLANK' className="col-md-12 mb-3" style={{color:"#000",textDecoration:'none'}}>
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{ job[3] ? ( <>{job[3]} ,</> ):( <></> )} {job[4]}</h5>
-                <p className="card-text">{job[1]}, {job[2]}</p>
-              </div>
-            </div>
-          </a>
-          <br/>
+    <div className="background-container" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover'}}>
+      <div className="container mt-4">
+        <h2 className="text-center mb-4" style={{  color: '#fff', padding: '10px' }}>List of Jobs</h2>
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            {jobs.map((job) => {
+              const datetimeObj = new Date(job[9]);
+              const formattedDate = datetimeObj.toLocaleString("en-US", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                hour12: false,
+              });
+
+              return (
+                <div key={job[0]} className="mb-4">
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>{job[7] ? `${job[7]}, ` : ''} {job[8]}</Card.Title>
+                      <Card.Text>Experience: {job[1]}</Card.Text>
+                      <Card.Text>Salary: {job[2]}</Card.Text>
+                      <Card.Text>Location: {job[4]}</Card.Text>
+                      <Card.Text>Posted on: {formattedDate}</Card.Text>
+                      <Button variant="primary" href={job[3]} target="_blank" style={{ animation: 'rainbow-text-animation 5s infinite' }}>Apply</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            })}
           </div>
-        ))}
-        
         </div>
       </div>
     </div>
